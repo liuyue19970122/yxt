693bfd9bec6ada6b3e4d30f9da3338cfbedf7ad1
xh4703 <470388378@qq.com>
2020-02-12 16:40:45 +0800

    Signed-off-by: xh4703 <470388378@qq.com>

--- pages/finance/statement/expense/expense.js
+++ pages/finance/statement/expense/expense.js
@@ -1,21 +1,104 @@
 // pages/finance/statement/income/income.js
+let util = require('../../../../utils/util.js');
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    price:0,
+    subPrice:'00',
+    curYear:'',
+    nowYear:'',
+    outcomeList:[]
   },
+  //picker时间事件
+  bindDateChange(e) { 
+    let year=e.detail.value
+    this.setData({
+      curYear:year
+    })
+    let data={
+      year:year
+    }
+    this.getOutcomeInfo(data)
+  },
+  bindDateCancel(e){},
   //查看详情
   bindClick(e){
-    console.log(e)
+    let month=e.detail
+    let curMonth=''
+    let list=this.data.outcomeList
+    list.forEach(item=>{
+      if(item.month===month){
+        curMonth=item.monthInfo
+      }
+    })
+    wx.navigateTo({
+      url: '/pages/finance/statement/detail/detail?month='+month+'&pageInType=outcome&curMonth='+curMonth,
+    })
+  },
+  //获取支出详情
+  getOutcomeInfo(data){
+    wx.showLoading({title:'加载中...'})
+    let url = app.globalData.baseUrl +'apiMall/report/outcome'
+    util.getRequestListData(url,data,false,this.outcomeInfoRes)
+  },
+  outcomeInfoRes(res,type){
+    wx.hideLoading()
+    if(res.data.code==='200'&& res.statusCode===200){
+      console.log(res)
+      let list=res.data.content
+      let price=0
+      list.forEach(item=>{
+        if(item.totalMoney){
+          item.cusTotalMoney=util.getMoney(item.totalMoney).toString()
+        }else{
+          item.cusTotalMoney=0
+        }
+        price=util.accAdd(price,item.totalMoney)
+      })
+      if(price){
+        list.forEach(item=>{
+          if(item.totalMoney){
+            let p=util.accDiv(item.totalMoney,price)
+            let pws=util.accMul(p,100)
+            item.cusPercent=pws+'%'
+          }else{
+            item.cusPercent='0%'
+          }
+        })
+      }else{
+        list.forEach(item=>{
+         item.cusPercent='0%'
+        })
+      }
+      let priceStr=util.getMoney(price).toString()
+      let priceArr=priceStr.split('.')
+      let intPrice=priceArr[0]
+      let litPrice=priceArr[1]
+      this.setData({
+        outcomeList:list,
+        price:intPrice,
+        subPrice:litPrice
+      })
+    }else{
+      wx.showToast({
+        title: res.data.message,
+      })
+    }
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let year = new Date().getFullYear()
+    let data={
+      year:year
+    }
+    this.setData({curYear:year,nowYear:year})
+    this.getOutcomeInfo(data)
   },
 
   /**
--- pages/finance/statement/income/income.js
+++ pages/finance/statement/income/income.js
@@ -1,21 +1,106 @@
 // pages/finance/statement/income/income.js
+let util = require('../../../../utils/util.js');
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    price:0,
+    subPrice:'00',
+    curYear:'',
+    nowYear:'',
+    incomeList:[]
+  },
+  //日期改变
+  bindDateChange(e){
+    console.log(e)
+    let year=e.detail.value
+    this.setData({
+      curYear:year
+    })
+    let data={
+      year:year
+    }
+    this.getIncomeInfo(data)
   },
   //查看详情
   bindClick(e){
+    let month=e.detail
+    let curMonth=''
+    let list=this.data.incomeList
+    list.forEach(item=>{
+      if(item.month===month){
+        curMonth=item.monthInfo
+      }
+    })
+    wx.navigateTo({
+      url: '/pages/finance/statement/detail/detail?month='+month+'&pageInType=income&curMonth='+curMonth,
+    })
     console.log(e)
+  },
+   //获取收入详情
+   getIncomeInfo(data){
+    wx.showLoading({title:'加载中...'})
+    let url = app.globalData.baseUrl +'apiMall/report/income'
+    util.getRequestListData(url,data,false,this.incomeInfoRes)
+  },
+  incomeInfoRes(res,type){
+    wx.hideLoading()
+    if(res.data.code==='200'&& res.statusCode===200){
+      let list=res.data.content
+      let price=0
+      list.forEach(item=>{
+        if(item.totalMoney){
+          item.cusTotalMoney=util.getMoney(item.totalMoney).toString()
+        }else{
+          item.cusTotalMoney=0
+        }
+        price=util.accAdd(price,item.totalMoney)
+      })
+      if(price){
+        list.forEach(item=>{
+          if(item.totalMoney){
+            let p=util.accDiv(item.totalMoney,price)
+           
+            let pws=util.accMul(p,100)
+            item.cusPercent=pws+'%'
+          }else{
+            item.cusPercent='0%'
+          }
+        })
+      }else{
+        list.forEach(item=>{
+         item.cusPercent='0%'
+        })
+      }
+      let priceStr=util.getMoney(price).toString()
+      let priceArr=priceStr.split('.')
+      let intPrice=priceArr[0]
+      let litPrice=priceArr[1]
+      this.setData({
+        incomeList:list,
+        price:intPrice,
+        subPrice:litPrice
+      })
+    }else{
+      wx.showToast({
+        title: res.data.message,
+      })
+    }
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let year = new Date().getFullYear()
+    let data={
+      year:year
+    }
+    this.setData({curYear:year,nowYear:year})
+    this.getIncomeInfo(data)
   },
 
   /**
